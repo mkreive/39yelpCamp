7104f457e87b84648b8641a4940aa21c921b06d1
mkreive <mkreive@gmail.com>
2022-01-14 17:08:09 +0200

    show

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const express = require("express");
+const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+
 const Campground = require("./models/campground");
 
 main().catch((err) => console.log(err));
+
 async function main() {
     await mongoose.connect("mongodb://localhost:27017/yelp-camp");
     console.log("MONGO CONNECTION OPEN!");
 }
 
-const app = express();
-app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -20,6 +26,11 @@ app.get("/", (req, res) => {
 app.get("/campground", async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render("campgrounds/index", { campgrounds });
+});
+
+app.get("/campgrounds/:id", async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/show", { campground });
 });
 
 app.listen(3000, () => {

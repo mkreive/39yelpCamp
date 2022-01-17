681055f9b3551bc0d227e50d42da0a20e1b239fc
mkreive <mkreive@gmail.com>
2022-01-17 15:21:35 +0200

    images

--- seeds/index.js
+++ seeds/index.js
@@ -15,9 +15,14 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state} `,
             title: `${sample(descriptors)} ${sample(places)}`,
+            image: "https://unsplash.com/collections/rScTgu5SO4g",
+            description:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure cum asperiores laudantium animi, quis enim, minus, sunt aliquid repellendus quos ducimus recusandae a possimus obcaecati laboriosam tempora non fuga repellat.",
+            price,
         });
         await camp.save();
     }

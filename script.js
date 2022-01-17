5f1c4d35b0bb24f6ff6e5705ce3eaab895addab4
mkreive <mkreive@gmail.com>
2022-01-17 09:55:56 +0200

    new and create

--- script.js
+++ script.js
@@ -28,6 +28,16 @@ app.get("/campground", async (req, res) => {
     res.render("campgrounds/index", { campgrounds });
 });
 
+app.get("/campgrounds/new", (req, res) => {
+    res.render("campgrounds/new");
+});
+
+app.post("/campgrounds", async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
 app.get("/campgrounds/:id", async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render("campgrounds/show", { campground });

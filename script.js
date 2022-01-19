6a51bcb00e00c26705d85baf2935a6df928c4d77
mkreive <mkreive@gmail.com>
2022-01-19 12:15:02 +0200

    more errors

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
 
 const Campground = require("./models/campground");
 
@@ -41,6 +42,8 @@ app.get("/campgrounds/new", (req, res) => {
 app.post(
     "/campgrounds",
     catchAsync(async (req, res, next) => {
+        if (!req.body.campground)
+            throw new ExpressError("Invalid Campground Data", 400);
         const campground = new Campground(req.body.campground);
         await campground.save();
         res.redirect(`/campgrounds/${campground._id}`);
@@ -84,8 +87,13 @@ app.delete(
 );
 
 // Error handling
+app.all("*", (req, res, next) => {
+    next(new ExpressError("PAGE NOT FOUND!!!!", 404));
+});
+
 app.use((err, req, res, next) => {
-    res.send("Oh boy, something went wrong!");
+    const { statusCode = 500, message = "Something went wrong..." } = err;
+    res.status(statusCode).send(message);
 });
 
 //App listener

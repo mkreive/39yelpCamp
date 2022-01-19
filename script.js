24d30df3ae982b01a52a42414eb3e932d0cce12e
mkreive <mkreive@gmail.com>
2022-01-19 12:47:36 +0200

    joi schema

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
+const Joi = require("joi");
 
 const Campground = require("./models/campground");
 
@@ -42,8 +43,19 @@ app.get("/campgrounds/new", (req, res) => {
 app.post(
     "/campgrounds",
     catchAsync(async (req, res, next) => {
-        if (!req.body.campground)
-            throw new ExpressError("Invalid Campground Data", 400);
+        // if (!req.body.campground)
+        //     throw new ExpressError("Invalid Campground Data", 400);
+        const campgroundSchema = Joi.object({
+            campground: Joi.object({
+                title: Joi.string().required(),
+                price: Joi.number().required().min(0),
+            }).required(),
+        });
+        const { error } = campgroundSchema.validate(req.body);
+        if (error) {
+            const msg = error.details.map((el) => el.message).join(",");
+            throw new ExpressError(msg, 400);
+        }
         const campground = new Campground(req.body.campground);
         await campground.save();
         res.redirect(`/campgrounds/${campground._id}`);

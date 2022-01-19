b82eab04104562f280fdc2a22370aa64166ca751
mkreive <mkreive@gmail.com>
2022-01-19 13:03:32 +0200

    JOI

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
-const Joi = require("joi");
+const { campgroundSchema } = require("./schemas.js");
 
 const Campground = require("./models/campground");
 
@@ -23,6 +23,16 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+};
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -42,20 +52,10 @@ app.get("/campgrounds/new", (req, res) => {
 
 app.post(
     "/campgrounds",
+    validateCampground,
     catchAsync(async (req, res, next) => {
         // if (!req.body.campground)
         //     throw new ExpressError("Invalid Campground Data", 400);
-        const campgroundSchema = Joi.object({
-            campground: Joi.object({
-                title: Joi.string().required(),
-                price: Joi.number().required().min(0),
-            }).required(),
-        });
-        const { error } = campgroundSchema.validate(req.body);
-        if (error) {
-            const msg = error.details.map((el) => el.message).join(",");
-            throw new ExpressError(msg, 400);
-        }
         const campground = new Campground(req.body.campground);
         await campground.save();
         res.redirect(`/campgrounds/${campground._id}`);
@@ -80,6 +80,7 @@ app.get(
 
 app.put(
     "/campgrounds/:id",
+    validateCampground,
     catchAsync(async (req, res) => {
         const { id } = req.params;
         const campground = await Campground.findByIdAndUpdate(id, {

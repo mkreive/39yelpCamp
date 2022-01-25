9db7997d133d4c7ed04b87ccad2f96cf6c23bf51
mkreive <mkreive@gmail.com>
2022-01-25 11:02:10 +0200

    register form

--- script.js
+++ script.js
@@ -11,8 +11,9 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user");
 
-const campgrounds = require("./routes/campgrounds");
-const reviews = require("./routes/reviews");
+const userRoutes = require("./routes/users");
+const campgroundRoutes = require("./routes/campgrounds");
+const reviewRoutes = require("./routes/reviews");
 
 main().catch((err) => console.log(err));
 
@@ -63,8 +64,9 @@ app.get("/fakeUser", async (req, res) => {
     res.send(newUser);
 });
 
-app.use("/campgrounds", campgrounds);
-app.use("/campgrounds/:id/reviews", reviews);
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
 
 // Home route
 app.get("/", (req, res) => {

e17ff24285fe36df546a394bc24cbfb9a24ae6e7
mkreive <mkreive@gmail.com>
2022-01-25 10:33:36 +0200

    passport configuration

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
 const campgrounds = require("./routes/campgrounds");
 const reviews = require("./routes/reviews");
@@ -41,10 +44,23 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
+});
+
+app.get("/fakeUser", async (req, res) => {
+    const user = new User({ email: "tetaOna@gmail.com", username: "tetaOna" });
+    const newUser = await User.register(user, "monkey");
+    res.send(newUser);
 });
 
 app.use("/campgrounds", campgrounds);

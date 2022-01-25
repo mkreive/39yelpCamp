8e72d01b92486b1221f839b106fd1ff57cab4953
mkreive <mkreive@gmail.com>
2022-01-25 12:31:59 +0200

    login logout

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 module.exports.isLogedIn = (req, res, next) => {
+    console.log("req.user...", req.user);
     if (!req.isAuthenticated()) {
         req.flash("error", "You must be signed in");
         return res.redirect("/login");

763b9077a36d2fb8629acb23e37de5a121d34b88
mkreive <mkreive@gmail.com>
2022-01-27 14:18:36 +0200

    'disapeared map'

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,12 +1,19 @@
-const campground = require("../../models/campground");
-
 mapboxgl.accessToken = mapToken;
 
 const map = new mapboxgl.Map({
-    container: "map", // container ID
-    style: "mapbox://styles/mapbox/streets-v11", // style URL
-    center: campground.geometrycoordinates,
-    zoom: 7, // starting zoom
+    container: "map",
+    style: "mapbox://styles/mapbox/light-v10", // stylesheet location
+    center: campground.geometry.coordinates, // starting position [lng, lat]
+    zoom: 10, // starting zoom
 });
 
-new mapboxgl.Marker().setLngLat(campground.geometrycoordinates).addTo(map);
+map.addControl(new mapboxgl.NavigationControl());
+
+new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 }).setHTML(
+            `<h3>${campground.title}</h3><p>${campground.location}</p>`
+        )
+    )
+    .addTo(map);

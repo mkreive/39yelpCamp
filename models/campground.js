2b2587b92bd46fbb770cf8fd7f12e472e1283047
mkreive <mkreive@gmail.com>
2022-01-27 14:45:33 +0200

    colt paste

--- models/campground.js
+++ models/campground.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
-const review = require("./review");
+const Review = require("./review");
 const Schema = mongoose.Schema;
+
+// https://res.cloudinary.com/douqbebwk/image/upload/w_300/v1600113904/YelpCamp/gxgle1ovzd2f3dgcpass.png
 
 const ImageSchema = new Schema({
     url: String,
@@ -11,38 +13,49 @@ ImageSchema.virtual("thumbnail").get(function () {
     return this.url.replace("/upload", "/upload/w_200");
 });
 
-const CampgroundSchema = new Schema({
-    title: String,
-    images: [ImageSchema],
-    geometry: {
-        type: {
-            type: String,
-            enum: ["Point"],
-            required: true,
+const opts = { toJSON: { virtuals: true } };
+
+const CampgroundSchema = new Schema(
+    {
+        title: String,
+        images: [ImageSchema],
+        geometry: {
+            type: {
+                type: String,
+                enum: ["Point"],
+                required: true,
+            },
+            coordinates: {
+                type: [Number],
+                required: true,
+            },
         },
-        coordinates: {
-            type: [Number],
-            required: true,
-        },
-    },
-    price: Number,
-    description: String,
-    location: String,
-    author: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-    },
-    reviews: [
-        {
+        price: Number,
+        description: String,
+        location: String,
+        author: {
             type: Schema.Types.ObjectId,
-            ref: "Review",
+            ref: "User",
         },
-    ],
+        reviews: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Review",
+            },
+        ],
+    },
+    opts
+);
+
+CampgroundSchema.virtual("properties.popUpMarkup").get(function () {
+    return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+    <p>${this.description.substring(0, 20)}...</p>`;
 });
 
 CampgroundSchema.post("findOneAndDelete", async function (doc) {
     if (doc) {
-        await review.remove({
+        await Review.deleteMany({
             _id: {
                 $in: doc.reviews,
             },

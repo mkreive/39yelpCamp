b96049d6e87a3021fadaf30de5e46f42d9041eba
mkreive <mkreive@gmail.com>
2022-01-27 14:00:34 +0200

    geolocation

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,8 +1,12 @@
+const campground = require("../../models/campground");
+
 mapboxgl.accessToken = mapToken;
 
 const map = new mapboxgl.Map({
     container: "map", // container ID
     style: "mapbox://styles/mapbox/streets-v11", // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
+    center: campground.geometrycoordinates,
     zoom: 7, // starting zoom
 });
+
+new mapboxgl.Marker().setLngLat(campground.geometrycoordinates).addTo(map);
